21bf62f0b9dc00a5251092d8326fea76ec22a257
GSM94 <shoma994@gmail.com>
2024-08-19 14:22:56 +0300

    sas

--- config/db.js
+++ config/db.js
@@ -1,31 +1,15 @@
 const mongoose = require('mongoose')
-const link = process.env.MONGODB_URI
-const dotenv = require('dotenv');
-dotenv.config()
+const link = 'mongodb+srv://shoma994:GeoWeb94@nuxt-blog.xycw8aw.mongodb.net/urlShortener?retryWrites=true&w=majority&appName=nuxt-blog'
 
-const connectDb = async () => {
-  console.log('Connecting to MongoDB');
-
-  try {
-    await mongoose.connect(link);
-    console.log('Successfully connected to MongoDB');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1); // Перепроверьте необходимость критического завершения
-  }
-
-  mongoose.connection.on('disconnected', () => {
-    console.log('MongoDB disconnected. Attempting to reconnect...');
-    connectDb(); // Попытка переподключения
-  });
-
-  mongoose.connection.on('reconnected', () => {
-    console.log('MongoDB reconnected.');
-  });
-
-  mongoose.connection.on('error', err => {
-    console.error(`MongoDB connection error: ${err}`);
-  });
+const connectDb = () => {
+	console.log('Connecting to MongoDB');
+	return mongoose.connect(link, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}).catch(err => { // Отловляем ошибки здесь
+		console.error('MongoDB connection error:', err);
+		throw err; // Пробрасываем ошибку дальше
+	});
 }
 
 module.exports = connectDb
