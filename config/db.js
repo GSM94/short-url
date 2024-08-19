3e7135da77fe9cfe62e71aa9bcdaa0231ae03374
GSM94 <shoma994@gmail.com>
2024-08-19 14:28:42 +0300

    цуй

--- config/db.js
+++ config/db.js
@@ -5,8 +5,27 @@ const connectDb = () => {
 	console.log('Connecting to MongoDB');
 	return mongoose.connect(link, {
 		useNewUrlParser: true,
-		useUnifiedTopology: true
-	}).catch(err => { // Отловляем ошибки здесь
+		useUnifiedTopology: true,
+		autoReconnect: true,
+		reconnectTries: Number.MAX_VALUE,
+		reconnectInterval: 1000
+	})
+	.then(() => {
+		console.log('Successfully connected to MongoDB');
+		// Дополнительная обработка событий соединения
+		mongoose.connection.on('disconnected', () => {
+			console.log('MongoDB disconnected. Attempting to reconnect...');
+		});
+
+		mongoose.connection.on('reconnected', () => {
+			console.log('MongoDB reconnected.');
+		});
+
+		mongoose.connection.on('error', err => {
+			console.error('MongoDB connection error:', err);
+		});
+	})
+	.catch(err => {
 		console.error('MongoDB connection error:', err);
 		throw err; // Пробрасываем ошибку дальше
 	});
